7dca8c88ada68d1530a615d3bbbdad57c801c774
oleksandrchekalenko <whibleykevin@gmail.com>
2023-07-08 22:39:12 +0300

    Add checkstyle configs

--- src/main/java/com/chelex/phonebook/PhoneBookApplication.java
+++ src/main/java/com/chelex/phonebook/PhoneBookApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PhoneBookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhoneBookApplication.class, args);
-	}
+    @SuppressWarnings("checkstyle:Indentation")
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneBookApplication.class, args);
+    }
 
 }
