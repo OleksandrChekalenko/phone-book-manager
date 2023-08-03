6fa9a32350703bad0d2d63bd526d4c1ab297439a
oleksandrchekalenko <whibleykevin@gmail.com>
2023-08-03 12:52:06 +0300

    Move phone-book-manager to a module

---
+++ spring-kafka-producer/src/main/java/com/chelex/SpringBootKafkaProducer.java
@@ -0,0 +1,10 @@
+package com.chelex;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootKafkaProducer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaProducer.class);
+    }
+}
